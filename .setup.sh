2449f25450e03acdcc6493d3d91c00c10c4a27cd
Austin J. Garrett <agarret7.mit.edu>
2017-09-01 23:16:08 -0400

    Add setup script.

---
+++ .setup.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# debian custom installation script
+
+username=agarret7
+
+#---------------------------------------------------#
+#-------- PACKAGE ------------- DESCRIPTION --------#
+#---------------------------------------------------#
+
+packages="apt-utils        "` # self-explanatory
+        `"audacity         "` # audio editing
+        `"cabal-debian     "` # haskell package manager
+        `"cmake            "` # build system for cxx
+        `"cmatrix          "` # puts the matrix in terminal
+        `"dropbox          "` # cloud storage
+        `"eclipse          "` # java IDE
+        `"feh              "` # background chooser
+        `"git              "` # software version control
+        `"haskell-platform "` # haskell language
+        `"i3-wm            "` # i3 window manager
+        `"i3blocks         "` # i3 status bar
+        `"kdenlive         "` # video editing
+        `"python-dev       "` # python 2.7 dev
+        `"python-pip       "` # python 2.7 package manager
+        `"python3.5        "` # python 3.5
+        `"python3-dev      "` # python 3.* dev
+        `"python3-pip      "` # python 3.* package manager
+        `"rxvt-unicode     "` # custom terminal emulator
+        `"skypeforlinux    "` # video caller
+        `"steam            "` # game distribution platform
+        `"sudo             "` # root access proxy
+        `"texlive-full     "` # latex language (typesetting)
+        `"texmaker         "` # latex IDE
+        `"unzip            "  # zip extraction
+
+python_lib="h5py           "` # binary data interchange format
+          `"matplotlib     "` # data visualization
+          `"numpy          "` # numerical data manipulation
+          `"pygame         "` # graphics interface
+          `"scipy          "  # scientific computing
+         
+dpkg --add-architecture i386
+
+apt-get update && apt-get upgrade
+apt-get install $packages
+
+pip2 install $python_lib
+pip  install $python_lib
+
+git clone https://github.com/agarret7/dotfiles.git ~
+
+adduser $username
